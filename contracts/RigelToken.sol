4108287ef3ac6534be1b1a548678aaa1950e4d8a
Avinash <avinash975@gmail.com>
2020-12-01 23:55:26 +0530

    Update RigelToken.sol

--- contracts/RigelToken.sol
+++ contracts/RigelToken.sol
@@ -1,14 +1,43 @@
 pragma solidity ^0.6.0;
 
-import  '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import  '@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol';
-import  '@openzeppelin/contracts/token/ERC20/TokenTimelock.sol';
+import  'github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+import  'github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Burnable.sol';
+import  'github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol';
 
 
+
+ contract RigelToken is ERC20Burnable, Ownable {
     
-    contract RigelToken is ERC20,ERC20Burnable,TokenTimelock {
     
-    constructor(uint256 initialSupply) ERC20("RigelToken", "RGP") public {
-        _mint(msg.sender, initialSupply);
+    constructor() ERC20("RigelToken", "RGP") public {
+        uint256 totalSupply = 2000000 * (10**18);
+        _mint(msg.sender, totalSupply);
     }
+    
+    /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `recipient` cannot be the zero address.
+     * - the caller must have a balance of at least `amount`.
+     */
+    
+     function transfer(address recipient, uint256 amount) public onlyOwner virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+    
+     /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See {ERC20-_burn}.
+     */
+     
+     function burn(uint256 amount) public onlyOwner virtual override{
+        _burn(_msgSender(), amount);
+    }
+    
+     
 }
+
